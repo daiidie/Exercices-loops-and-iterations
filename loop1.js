fb48630d9bee1cbebf2ede1e2d7e9e5b46df6bcc
daiidie <delavegae46@gmail.com>
2021-03-01 01:49:50 +0100

    first try not ez

---
+++ loop1.js
@@ -0,0 +1,8 @@
+  
+let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let i = 0
+
+while (i < tab.length) {
+  i++
+  console.log(i)
+}
---
+++ threeWayCountDown.js
@@ -0,0 +1,24 @@
+for (let i = 101; i >= 0; --i) {
+  if (i % 2 === 0) {
+    continue
+  }
+  console.log(i)
+}
+console.log("..............")
+
+for (let j = 101; j >= 0; --j) {
+  if (j % 2 !== 1) {
+    continue
+  }
+   
+  console.log(j)
+}
+
+console.log("..............")
+ 
+for (let k = 101; k >= 0; --k) {
+  if (k % 2 === 0) {
+    continue
+  }
+  console.log(k)
+}
